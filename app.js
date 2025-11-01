d1462464c5bab28025dec07e08f833804cbf9ce7
vikasjas <67204103+vikasjas@users.noreply.github.com>
2025-11-01 13:14:12 -0400

    hghghgddgdg

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Smooth fade-in on load
+// Smooth fade on load
 document.body.style.opacity = "0";
 window.addEventListener("load", () => {
   document.body.style.transition = "opacity 1.5s ease";
